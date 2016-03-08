096e6f800e8f14c7641734ba5d592315b3d0eb8b
Taylor Nycum <taylor.nycum@gmail.com>
2016-03-07 20:07:51 -0500

    Added UI skeleton.

--- app/src/main/java/com/example/taylor/activityrecognition/MainActivity.java
+++ app/src/main/java/com/example/taylor/activityrecognition/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.taylor.activityrecognition;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView contentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contentView = (ListView) findViewById(R.id.contentView);
+    }
+
+    private void setCurrentActivity(String activityName) {
+        TextView curActivityText;
+        curActivityText = (TextView) findViewById(R.id.curActivityText);
+        curActivityText.setText(activityName);
     }
 }
